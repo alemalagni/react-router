62ae3f77a26e65552d4f6ebf231ea32d26488a99
alemalagni <alemalagni@gmail.com>
2025-04-16 16:56:13 +0200

    feat(app): add

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import './App.css';
 import HomePage from './pages/HomePage.jsx';
 import AboutUs from './pages/AboutUs.jsx';
 import PostList from './pages/PostList.jsx';
+import MainNav from './components/MainNav.jsx';
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/AboutUs" element={<AboutUs />} />
-      <Route path="/PostList" element={<PostList />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <>
+      <MainNav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/PostList" element={<PostList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
