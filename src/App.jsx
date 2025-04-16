975b2dd984629315496eea521fe84449882e05e2
alemalagni <alemalagni@gmail.com>
2025-04-16 16:40:57 +0200

    feat(app): add <BrowserRouter>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import HomePage from './pages/HomePage.jsx'
 
 function App() {
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
