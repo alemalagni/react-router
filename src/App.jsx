ba2d45c575e115d6809703ff032297548bbe8f1d
alemalagni <alemalagni@gmail.com>
2025-04-16 16:53:32 +0200

    feat(app): add AboutUs and PostList routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import HomePage from './pages/HomePage.jsx'
+import './App.css';
+import HomePage from './pages/HomePage.jsx';
+import AboutUs from './pages/AboutUs.jsx';
+import PostList from './pages/PostList.jsx';
 
 function App() {
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/AboutUs" element={<AboutUs />} />
+      <Route path="/PostList" element={<PostList />} />
     </Routes>
   </BrowserRouter>
 }
