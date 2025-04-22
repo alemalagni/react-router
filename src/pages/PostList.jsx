bfe67a996565ac05caf3a34cfbafe8a1f695f69f
alemalagni <alemalagni@gmail.com>
2025-04-22 17:07:40 +0200

    feat(PostList): add fetch function

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -2,4 +2,14 @@ import { useState, useEffect } from "react";
 
 export default function PostList() {
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res => res.json())
+    }, []);
+
+    return (
+        <div>
+        </div>
+    );
 }
